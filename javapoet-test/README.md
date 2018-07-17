58bc219864e37b173fa70f4650e297765776ff48
along101 <yzl1414114@163.com>
2018-07-17 15:28:08 +0800

    增加 javapoet-test 模块

---
+++ javapoet-test/README.md
@@ -0,0 +1,7 @@
+
+`JavaPoet` 生成 `.java` 源文件的 java API.
+
+
+参考：
+
+https://github.com/square/javapoet
---
+++ javapoet-test/src/test/java/com/yzl/javapoet/test/TypeSpecTest.java
@@ -0,0 +1,35 @@
+package com.yzl.javapoet.test;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import org.junit.Test;
+
+import javax.lang.model.element.Modifier;
+
+
+/**
+ * @author yinzuolong
+ */
+public class TypeSpecTest {
+
+    @Test
+    public void test() throws Exception {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+        javaFile.writeTo(System.out);
+    }
+}
