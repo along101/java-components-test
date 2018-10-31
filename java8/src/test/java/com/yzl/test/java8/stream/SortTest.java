34602c0e8d49e079edda495f91d3ad283dcd9335
yutu <zuolong.yzl@alibaba-inc.com>
2018-11-01 02:02:04 +0800

    增加排序写法

---
+++ java8/src/test/java/com/yzl/test/java8/stream/SortTest.java
@@ -0,0 +1,37 @@
+package com.yzl.test.java8.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author yutu
+ * @date 2018/11/1
+ */
+public class SortTest {
+
+    @Test
+    public void test1() {
+        List<String> a = new ArrayList<>(Arrays.asList("1", "3", "4", "2", "5"));
+
+        List<String> b = new ArrayList<>(a);
+        b.sort(Comparator.comparing(s -> s));
+        System.out.println(b);
+
+        b = new ArrayList<>(a);
+        b.sort(String::compareTo);
+        System.out.println(b);
+
+
+        b = new ArrayList<>(a);
+        b.sort(Comparator.comparing(String::hashCode));
+        System.out.println(b);
+
+        b = a.stream().sorted(String::compareTo).collect(Collectors.toList());
+        System.out.println(b);
+    }
+}
