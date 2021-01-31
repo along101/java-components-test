e10465f327291f644224b5e424381c8c0167042d
yutu <zuolong.yzl@alibaba-inc.com>
2021-01-31 23:00:44 +0800

    增加注释

--- mock/src/test/java/com/yzl/test/mockito/MockitoTest.java
+++ mock/src/test/java/com/yzl/test/mockito/MockitoTest.java
@@ -3,8 +3,6 @@ package com.yzl.test.mockito;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Matchers;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
@@ -22,12 +20,13 @@ public class MockitoTest {
 
     @Test
     public void simpleTest() {
-        //arrange
+        //先mock需要的对象
         Iterator i = mock(Iterator.class);
+        //给mock对象设置执行的结果
         when(i.next()).thenReturn("Hello").thenReturn("World");
-        //act
+        //对象方法执行
         String result = i.next() + " " + i.next();
-        //verify
+        //mock的verify验证
         verify(i, times(2)).next();
         //assert
         assertEquals("Hello World", result);
@@ -47,6 +46,7 @@ public class MockitoTest {
 
     @Test(expected = IOException.class)
     public void when_thenThrow() throws IOException {
+        //抛出异常测试
         OutputStream outputStream = mock(OutputStream.class);
         //预设当流关闭时抛出异常
         doThrow(new IOException()).when(outputStream).close();
@@ -55,9 +55,10 @@ public class MockitoTest {
 
     @Test
     public void argumentMatchersTest() {
-        List<String> mock = mock(List.class);
+        List mock = mock(List.class);
         when(mock.get(anyInt())).thenReturn("Hello").thenReturn("World");
         String result = mock.get(100) + " " + mock.get(200);
+        //验证mock对象的get方法被调用了2次
         verify(mock, times(2)).get(anyInt());
         assertEquals("Hello World", result);
     }
@@ -67,12 +68,14 @@ public class MockitoTest {
         Map mapMock = mock(Map.class);
         when(mapMock.put(anyInt(), anyString())).thenReturn("world");
         mapMock.put(1, "hello");
+
+        //验证put方法执行，如果没有按照下面参数执行，则验证失败
         verify(mapMock).put(anyInt(), eq("hello"));
         verify(mapMock).put(1, "hello");
     }
 
     @Test
-    public void verifyTestTest() {
+    public void verifyTest() {
         List<String> mock = mock(List.class);
         List<String> mock2 = mock(List.class);
         when(mock.get(0)).thenReturn("hello");
@@ -80,39 +83,52 @@ public class MockitoTest {
         mock.get(1);
         mock.get(2);
         mock2.get(0);
+        //验证mock对象get(2)方法被执行了
         verify(mock).get(2);
+        //验证mock对象没有执行过get(3)方法
         verify(mock, never()).get(3);
     }
 
     @Test
     public void customAnswerTest() {
+        //Answer函数返回mock方法的调用
         List<String> mock = mock(List.class);
-        when(mock.get(4)).thenAnswer(new Answer() {
-            public String answer(InvocationOnMock invocation) throws Throwable {
-                Object[] args = invocation.getArguments();
-                Integer num = (Integer) args[0];
-                if (num > 3) {
-                    return "yes";
-                } else {
-                    throw new RuntimeException();
-                }
+        when(mock.get(4)).thenAnswer(invocation -> {
+            Object[] args = invocation.getArguments();
+            Integer num = (Integer) args[0];
+            if (num > 3) {
+                return "yes";
+            } else {
+                throw new RuntimeException();
             }
         });
         Assert.assertEquals("yes", mock.get(4));
+
+        doAnswer(invocation -> {
+            Object[] args = invocation.getArguments();
+            Integer num = (Integer) args[0];
+            return "test" + num;
+        }).when(mock).get(anyInt());
+        Assert.assertEquals("test2", mock.get(2));
     }
 
     @Test
     public void argumentCaptorTest() {
+        //捕获mock方法调用的参数
         List<String> mock = mock(List.class);
         List<String> mock2 = mock(List.class);
         mock.add("John");
         mock2.add("Brian");
         mock2.add("Jim");
+        //参数捕获器
         ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
+        //捕获第一次调用的参数
         verify(mock).add(argument.capture());
         assertEquals("John", argument.getValue());
+        //捕获第二次调用的参数
         verify(mock2, times(2)).add(argument.capture());
         assertEquals("Jim", argument.getValue());
+        //所有捕获的参数
         assertArrayEquals(new Object[]{"John","Brian", "Jim"}, argument.getAllValues().toArray());
     }
 
@@ -125,7 +141,7 @@ public class MockitoTest {
         Map<String,String> mock = mock(Map.class);
 
         when(mock.get(anyString())).thenAnswer((Answer<String>) invocation -> {
-            String arg = invocation.getArgumentAt(0,String.class);
+            String arg = invocation.getArgumentAt(0, String.class);
             return myMap.get(arg);
         });
 
